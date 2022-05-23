1dc94edd4e2ee4077d68368c231947e84ca52f30
bookworm41 <95772102+aman5213@users.noreply.github.com>
2022-05-23 14:24:41 +0000

    i made it so it finds 1000 in 2 seconds

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
+import time
+from math import ceil
 list = []
 count = 1
+isNotPrime=False
 number = int(input('''please enter the prime number you wish to find.
 >'''))
 numberOfMultipliers=0
 number+=1
+print('...')
+timea=time.perf_counter()
 while True:
   
-  for i in range(1,count+1):
+  for i in range(1,ceil((count+1)/2)):
+    if numberOfMultipliers==2:
+      break
     if count%i==0:
       numberOfMultipliers+=1
       
-  if numberOfMultipliers<3:
+  if numberOfMultipliers<2:
     list.append(count)  
   else:
     pass
+
     
   numberOfMultipliers=0
   count += 1
-  print("Count="+ str(count))
   if len(list)==number:
     break
-print(list)
+print(list[number-1])
+timeb=time.perf_counter()
+print('it took about',round(timeb-timea),'seconds.')
